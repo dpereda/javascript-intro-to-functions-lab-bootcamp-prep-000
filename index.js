8a49566d8015bb3d95642c1f0e83dde0dab30cdd
Dan  Pereda <dpereda@gmail.com>
2018-02-27 18:38:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()) {
     return "I can\'t hear you!"
+  } else (string === string.toUpperCase()) {
+    return "I can\'t hear you!"
   }
   
   
