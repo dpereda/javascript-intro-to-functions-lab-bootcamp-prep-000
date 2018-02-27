bafdb266a51acaa18270184e0f87d9bf931187ce
Dan  Pereda <dpereda@gmail.com>
2018-02-27 18:41:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function sayHiToGrandma(string){
     return "I can\'t hear you!"
   } else if (string === string.toUpperCase()) {
     return "YES INDEED"
-  } else
+  } else if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
   
   
   return string.toLowerCase()
